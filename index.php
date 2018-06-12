fd4aa2864ee2759448313a7be11e4744f7fabf57
Edson Feimberg <feimberg@gmail,com>
2018-06-12 09:40:02 -0300

    Iniciado parte visual do index com bootstrap 3

--- index.php
+++ index.php
@@ -1,7 +1,51 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Feimberg
- * Date: 12/06/2018
- * Time: 08:30
- */
+<html>
+
+<head>
+    <title>Classificados</title>
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <script type="text/javascript" src="assets/js/jquery-3.3.1.min.js"></script>
+    <script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="assets/js/script.js"></script>
+</head>
+
+<body>
+
+<nav class="navbar navbar-inverse">
+    <div class="container-fluid">
+        <div class="navbar=header">
+            <a href="./" class="navbar-brand">Classificados</a>
+        </div>
+
+        <ul class="nav navbar-nav navbar-right">
+            <li><a href="">Cadastre-se</a></li>
+            <li><a href="">Login</a></li>
+        </ul>
+    </div>
+
+</nav>
+
+<div class="container">
+
+    <div class="jumbotron">
+        <h1> Nós temos hoje 900000 anúncios</h1>
+        <p>E mais de 58273 usuários cadastrados</p>
+
+        <div class="row">
+            <div class="col-sm-3">
+                <h4>Pesquisa Avançada</h4>
+            </div>
+
+            <div class="col-sm-9">
+                <h4>Últimos Anúncios</h4>
+            </div>
+        </div>
+    </div>
+
+</div>
+
+
+</body>
+
+
+</html>
